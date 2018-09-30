1a8655419cd1df3eea813b422133440f93af3f5c
pragnya1996 <42793178+pragnya1996@users.noreply.github.com>
2018-09-30 20:04:20 +0530

    Add files via upload

---
+++ demo-olx/demo-olx/src/main/java/com/olx/demo/demoolx/DemoOlxApplication.java
@@ -0,0 +1,12 @@
+package com.olx.demo.demoolx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoOlxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoOlxApplication.class, args);
+	}
+}
---
+++ demo-olx/src/test/java/com/olx/demo/demoolx/RegisterControllerTest.java
@@ -0,0 +1,116 @@
+package com.olx.demo.demoolx;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+
+
+
+
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class RegisterControllerTest {
+@Mock
+RegisterService registerService;
+
+@InjectMocks
+RegisterController registerController;
+ @Test
+ public void addRegister1() {
+	 RegisterPojo register=new RegisterPojo("pragnya","abc","123456","abc@gmail.com","9876543210","123","free",true);
+	 Mockito.when(registerService.addregister(register)).thenReturn(register);
+	 RegisterPojo reg1=registerController.addNewRegister(register);
+		//System.out.println(reg1);
+		Assert.assertNotNull(reg1);
+		
+ }
+ @Test
+ public void addRegister2() {
+	 RegisterPojo register=new RegisterPojo("pragnya","abc","123","abc@gmail.com","1287","123","free",true);
+	 Mockito.when(registerService.addregister(register)).thenReturn(register);
+	 RegisterPojo reg1=registerController.addNewRegister(null);
+		//System.out.println(reg1);
+		Assert.assertNotNull(reg1);
+		}
+ @Test
+	public void getAllProductTest_1()
+	{
+		List<RegisterPojo> regList=new ArrayList<RegisterPojo>();
+		RegisterPojo register=new RegisterPojo("pragnya","abc","123456","abc@gmail.com","9876543210","123","free",true);
+		regList.add(register);
+		register=new RegisterPojo("pragnya","abc","123456","abc@gmail.com","9876543210","123","free",true);
+		regList.add(register);
+		Mockito.when(registerService.findAll()).thenReturn(regList);
+		List<RegisterPojo> registers=registerController.findAll();
+		Assert.assertEquals(regList.size(),registers.size());
+ 
+}
+ @Test
+ public void updateReg_1()
+	{
+		RegisterPojo register1=new RegisterPojo("pragnya","abc","123456","abc@gmail.com","9876543210","123","free",true);
+		Mockito.when(registerService.updateregister(register1)).thenReturn(register1);
+		RegisterPojo reg1=registerController.updateRegister(register1);
+		Assert.assertNotNull(reg1);
+		
+	}
+	@Test
+	public void updateReg_2()
+	{
+		RegisterPojo register1=new RegisterPojo("pragnya","abc","123456","abc@gmail.com","9876543210","123","free",true);
+		Mockito.when(registerService.updateregister(register1)).thenReturn(register1);
+		RegisterPojo reg1=registerController.updateRegister(null);
+		Assert.assertNotNull(reg1);
+		
+	}
+	@Test
+	 public void findByPhone1()
+		{
+			RegisterPojo register2=new RegisterPojo();
+			register2.setPhoneNo("9036851911");
+			Mockito.when(registerService.findByPhoneNo("9036851911")).thenReturn(register2);
+			RegisterPojo reg1=registerController.getPhoneNo("9036851911");
+			Assert.assertNotNull(reg1);
+			
+		}
+	@Test
+	 public void findByPhone2()
+		{
+			RegisterPojo register2=new RegisterPojo();
+			register2.setPhoneNo("9036851911");
+			Mockito.when(registerService.findByPhoneNo("9036851911")).thenReturn(register2);
+			RegisterPojo reg1=registerController.getPhoneNo(null);
+			Assert.assertNotNull(reg1);
+			
+		}
+	
+	@Test
+	 public void findByEmail1()
+		{
+			RegisterPojo register2=new RegisterPojo();
+			register2.setEmailId("prabhu@gmail.com");
+			Mockito.when(registerService.findByEmailId("prabhu@gmail.com")).thenReturn(register2);
+			RegisterPojo reg1=registerController.getEmailId(null);
+			Assert.assertNotNull(reg1);
+			
+		}
+	@Test
+	 public void findByEmail2()
+		{
+			RegisterPojo register2=new RegisterPojo();
+			register2.setEmailId("prabhu@gmail.com");
+			Mockito.when(registerService.findByEmailId("prabhu@gmail.com")).thenReturn(register2);
+			RegisterPojo reg1=registerController.getEmailId("prabhu@gmail.com");
+			Assert.assertNotNull(reg1);
+			
+		}
+}
